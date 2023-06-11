62835d35633463dacf36873289a85112d30240ea
Fizzx <botfizz285@gmail.com>
2023-06-11 22:19:59 +0700

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,9 +1,9 @@
-BOT_TOKEN = '**********:**************************'
+BOT_TOKEN = '6074549583:AAEMGBQXrPDWyIk_EsFZDJylZVfVm_1-Ohs'
 
-PROJECT_NAME = 'store-bot-example'
+PROJECT_NAME = 'Mbot-Store'
 
 WEBHOOK_HOST = f"https://{PROJECT_NAME}.herokuapp.com"
 WEBHOOK_PATH = '/webhook/' + BOT_TOKEN
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
-ADMINS = [000000000, 1234567890]
+ADMINS = [6287882471084, 6287882471084]
